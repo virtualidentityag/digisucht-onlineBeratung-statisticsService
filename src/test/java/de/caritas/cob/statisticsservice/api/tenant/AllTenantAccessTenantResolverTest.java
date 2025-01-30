731890fcdee17439cd0bec0afd375f357d2bdefd
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-01-30 17:42:46 +0100

    fix: keycloak authentication token class cast exceptions

--- src/test/java/de/caritas/cob/statisticsservice/api/tenant/AllTenantAccessTenantResolverTest.java
+++ src/test/java/de/caritas/cob/statisticsservice/api/tenant/AllTenantAccessTenantResolverTest.java
@@ -4,8 +4,13 @@ package de.caritas.cob.statisticsservice.api.tenant;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import jakarta.servlet.http.HttpServletRequest;
-import org.assertj.core.util.Sets;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
@@ -15,6 +20,10 @@ import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 @ExtendWith(MockitoExtension.class)
 class AllTenantAccessTenantResolverTest {
@@ -31,16 +40,20 @@ class AllTenantAccessTenantResolverTest {
   @Mock
   Access access;
 
+  @Mock
+  SecurityContext mockSecurityContext;
+
+  @Mock
+  Authentication mockAuthentication;
+
   @InjectMocks
   AllTenantAccessTenantResolver allTenantAccessTenantResolver;
 
   @Test
   void resolve_should_ResolveTechnicalTenantId_ForTenantSuperAdminUserRole() {
     // given
-    when(authenticatedRequest.getUserPrincipal()).thenReturn(token);
-    when(token.getAccount()
-        .getKeycloakSecurityContext().getToken()).thenReturn(accessToken);
-    when(accessToken.getRealmAccess().getRoles()).thenReturn(Sets.newLinkedHashSet("tenant-admin"));
+    givenUserIsAuthenticated();
+    when(mockAuthentication.getPrincipal()).thenReturn(buildJwtWithRealmRole("tenant-admin"));
     var resolved = allTenantAccessTenantResolver.resolve(authenticatedRequest);
     // then
     assertThat(resolved).contains(TECHNICAL_CONTEXT);
@@ -49,12 +62,37 @@ class AllTenantAccessTenantResolverTest {
   @Test
   void resolve_should_NotResolveTenantId_When_NonTechnicalUserRole() {
     // given
-    when(authenticatedRequest.getUserPrincipal()).thenReturn(token);
-    when(token.getAccount()
-        .getKeycloakSecurityContext().getToken()).thenReturn(accessToken);
-    when(accessToken.getRealmAccess().getRoles()).thenReturn(Sets.newLinkedHashSet("another-role"));
+    givenUserIsAuthenticated();
+    when(mockAuthentication.getPrincipal()).thenReturn(buildJwtWithRealmRole("another-role"));
     var resolved = allTenantAccessTenantResolver.resolve(authenticatedRequest);
     // then
     assertThat(resolved).isEmpty();
+  }
+
+
+  @AfterEach
+  public void tearDown() {
+    SecurityContextHolder.clearContext();
+  }
+
+  private void givenUserIsAuthenticated() {
+    SecurityContextHolder.setContext(mockSecurityContext);
+    when(mockSecurityContext.getAuthentication()).thenReturn(mockAuthentication);
+  }
+
+  private Jwt buildJwtWithRealmRole(String realmRole) {
+    Map<String, Object> headers = new HashMap<>();
+    headers.put("alg", "HS256");
+    headers.put("typ", "JWT");
+    return new Jwt(
+        "token", Instant.now(), Instant.now().plusMillis(1), headers, givenClaimMapContainingRole(realmRole));
+  }
+
+  private HashMap<String, Object> givenClaimMapContainingRole(String realmRole) {
+    HashMap<String, Object> claimMap = Maps.newHashMap();
+    var realmAccess = Maps.newHashMap();
+    realmAccess.put("roles", Lists.newArrayList(realmRole));
+    claimMap.put("realm_access", realmAccess);
+    return claimMap;
   }
 }
